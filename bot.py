304d90ade22f3b1024fc350ebf77f1b23f59011e
Cody Paschich <professional.voidlurker.net>
2018-12-23 03:09:20 -0800

    Added configuration options and reload command

--- bot.py
+++ bot.py
@@ -1,18 +1,40 @@
 import discord
 import asyncio
-#import sqlite3
+import configparser
+import random
+import os.path
 
+#load config ==============================================
+def init_config():
+    global botConfig
+    botConfig = configparser.ConfigParser()
+    botConfig['BOT_SETTINGS'] = {'TossRole': 'roleban', 'MinDeleteTime': 8, 'MaxDeleteTime': 12 }
+    botConfig['CHANNELS'] = {'ModChat': 'mod-chat', 'QuickDelete': ""}
+                    
+    if os.path.isfile("config.ini"):         
+        botConfig.read("config.ini")
+    else:
+        with open("config.ini", "w") as configfile:
+            botConfig.write(configfile)
+
+    return True;
+
+if not init_config():
+    print("Config error.")
+
+#variables ================================================
 client = discord.Client()
-
 global quickdelete_list
 quickdelete_list = {}
-
 global channel_modchat
+global channel_quickdelete
 global guild_home
 
+#functions ================================================
 def get_roleban_role(guild):
+    global botConfig
     for role in guild.roles:
-        if role.name == "roleban":
+        if role.name == botConfig['BOT_SETTINGS']['TossRole']:
             return role
     return None
 
@@ -25,12 +47,29 @@ def locate_channel(guild, channelName):
 def remove_prefix(text, prefix):
     return text[text.startswith(prefix) and len(prefix):]
 
+def apply_config():
+    global guild_home
+    global channel_modchat;
+    channel_modchat = locate_channel(guild_home, botConfig['CHANNELS']['ModChat']);
+    if (channel_modchat == None):
+        print("No " + botConfig['CHANNELS']['ModChat'] + " located in " + guild_home.name + ".");
+    else:
+        print(botConfig['CHANNELS']['ModChat'] + " located.")
+        
+    global channel_quickdelete
+    channel_quickdelete = locate_channel(guild_home, botConfig['CHANNELS']['QuickDelete']);
+    if (channel_quickdelete == None):
+        print("No " + botConfig['CHANNELS']['QuickDelete'] + " located in " + guild_home.name + ".");
+    else:
+        print(botConfig['CHANNELS']['QuickDelete'] + " located.")
+      
+    return True
+#events ===================================================
 @client.event
 async def on_ready():
     print('logged in'.format(client))
     act = discord.Activity(type=discord.ActivityType.watching, name="for =help")
     await client.change_presence(activity=act)
-    
     
     global guild_home
     guild_home = None
@@ -39,12 +78,8 @@ async def on_ready():
         guild_home = guild;
         break;
         
-    global channel_modchat;
-    channel_modchat = locate_channel(guild, "mod-chat");
-    if (channel_modchat == None):
-        print("No mod-chat located in " + guild_home.name + ".");
-    else:
-        print("mod-chat located.")
+    apply_config()
+    
 
 @client.event
 async def on_message(message):
@@ -53,6 +88,7 @@ async def on_message(message):
     
     global quickdelete_list
     global channel_modchat
+    global botConfig
     
     if message.content.startswith("=ping"):
         await message.channel.send("pong!")
@@ -68,9 +104,10 @@ Currently does not support images.\
 Can only be used by users with the manage_roles permission.\
 \n`=untoss` - Removes the `roleban` role from mentioned users (and will restore their roles when role \
 removal is implemented).\
-\n`=timer` - creates a 3 minute timer, which pings the user when finished. Primarly meant for moderation purposes, \
+\n`=timer` - Creates a 3 minute timer, which pings the user when finished. Primarly meant for moderation purposes, \
 but can be used by normal users if they have a reason. \
-\n`=purge` - attempts to delete all messages in the channel. Requires manage_messages permission.\
+\n`=purge` - Attempts to delete all messages in the channel. Requires manage_messages permission.\
+\n`=reload` - Reloads the bot's config file. Requires manage_server permission.\
 \n\n__**Passive Features**__\
 \nQuickdelete police - The bot will report messages that are deleted too quickly after being \
 posted directly to the mod team. If there is no mod channel, the message will instead be reposted \
@@ -149,12 +186,14 @@ in the channel it was deleted from.\
         else:
             await message.channel.send("You need manage_messages to use this command.")
         return
+        
     elif message.content.startswith("=timer"):
         await message.channel.send(":hourglass_flowing_sand:")
         print("set timer")
         await asyncio.sleep(180)
         msg = await message.channel.send(message.author.mention)
         await msg.edit(content=":hourglass:")
+        
     elif message.content.startswith("=report"):
         await message.delete()
         if channel_modchat != None:
@@ -162,11 +201,21 @@ in the channel it was deleted from.\
             await channel_modchat.send(report_message)
         else:
             await message.channel.send("This server currently doesn't accept anonymous reports.")
-        
+    elif message.content.startswith("=reload"):
+        async with message.channel.typing():
+            perms = message.author.permissions_in(message.channel)
+            if perms.manage_guild:
+                if init_config() and apply_config():
+                    await message.channel.send("Reloaded config!")
+                else:
+                    await message.channel.send("There was an error loading the config.")
+            else:
+                await message.channel.send("You don't have permission to do that!")
+            
     # quickdelete police
     if not message.author.bot:
         quickdelete_list[message.id] = message
-        await asyncio.sleep(10)
+        await asyncio.sleep(random.randint(int(botConfig['BOT_SETTINGS']['MinDeleteTime']), int(botConfig['BOT_SETTINGS']['MaxDeleteTime'])))
         quickdelete_list.pop(message.id, None)
 
 @client.event
@@ -184,8 +233,9 @@ async def on_message_delete(message):
             deletionEmbed.set_author(name=message.author.name + " said...", icon_url=message.author.avatar_url) 
             
             global channel_modchat
-            if channel_modchat == None:
-                deletionEmbed.set_footer(text="This message was automatically re-sent because it was deleted too recently after it was sent. Please ask an administrator if you would like the post removed entirely.")
+            if channel_quickdelete == None:
+                deletionEmbed.set_footer(text="This message was automatically re-sent because it was deleted too recently \
+                after it was sent. Please ask an administrator if you would like the post removed entirely.")
                 await message.channel.send(embed=deletionEmbed)
             else:
                 #TODO: add reacts to auto post in original channel
